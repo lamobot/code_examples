69b6992a1850ad837b44c4a580886c1f970267c0
lamobot <49595785+lamobot@users.noreply.github.com>
2021-01-29 08:57:45 +0300

    Delete python directory

--- python/parse_zabbix_sql/config.py
+++
@@ -1,9 +0,0 @@
-# Global variables
-filename: str = 'test.csv'
-spliter:  str = ';'
-
-# connect to zk database
-zabbix_db_hostname = ''
-zabbix_db_username = ''
-zabbix_db_password = ''
-zabbix_db_name = ''
